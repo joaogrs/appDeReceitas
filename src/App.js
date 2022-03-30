9a51e71ce76fad1ca8cb52107ceda9aef8eeb127
fernando <pcabral.fernando@gmail.com>
2022-03-29 21:08:18 -0300

    requisitos Login 1 a 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
+import Foods from './Pages/Foods';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ Login } />
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/foods" component={ Foods } />
+      </Switch>
     </BrowserRouter>
   );
 }
