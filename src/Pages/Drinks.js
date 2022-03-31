6940180b9c5934192977378ad49e750d2ba2a83d
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-03-31 18:09:55 -0300

    Requisitos 25 e 26

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import CardDrink from '../Components/CardDrink';
+import myContext from '../Context/myContext';
+import { fetchApi } from '../Helpers/useFetch';
 
 function Drinks(props) {
+  const { setApiData } = useContext(myContext);
+  useEffect(() => {
+    (async () => {
+      const endpointInitial = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+      const drinksInitial = await fetchApi(endpointInitial);
+      console.log(drinksInitial);
+      setApiData(drinksInitial);
+    })();
+  }, []);
   return (
     <section>
       <h1>Drinks</h1>
--- src/Pages/Foods.js
+++ src/Pages/Foods.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import CardMeal from '../Components/CardMeal';
+import { fetchApi } from '../Helpers/useFetch';
+import myContext from '../Context/myContext';
 
 function Foods(props) {
+  const { setApiData } = useContext(myContext);
+  useEffect(() => {
+    (async () => {
+      const endpointInitial = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const foodsInitial = await fetchApi(endpointInitial);
+      console.log(foodsInitial);
+      setApiData(foodsInitial);
+    })();
+  }, []);
   return (
     <section>
       <h1>Foods</h1>
