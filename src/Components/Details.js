ee988654cb043bf14d1cbbc9f64ab0458fd2d654
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-04-02 16:44:33 -0300

    Requisito 43- botão de share

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import copy from 'clipboard-copy';
 import IngredientsList from './IngredientsList';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function Details({ dataOfDetails, path }) {
+  const [btnShareshow, setBtnShareShow] = useState(true);
+  // const LINK = 'Link copied!';
+  const TWO_SECONDS = 2000;
+  const clickShareRecipe = async (object) => {
+    if (Object.keys(object)[0] === 'idMeal') {
+      await copy(`http://localhost:3000/foods/${object.idMeal}`);
+      // global.alert(LINK);
+      setBtnShareShow(false);
+      setTimeout(() => {
+        setBtnShareShow(true);
+      }, TWO_SECONDS);
+    }
+    if (Object.keys(object)[0] === 'idDrink') {
+      await copy(`http://localhost:3000/drinks/${object.idDrink}`);
+      // global.alert(LINK);
+      setBtnShareShow(false);
+      setTimeout(() => {
+        setBtnShareShow(true);
+      }, TWO_SECONDS);
+    }
+  };
+
+  const clickFavoriteRecipe = () => {
+    console.log('Clicou Favoritar');
+  };
+
   return (
     path.includes('drinks') ? (
       <section data-testid="recipe-details">
@@ -12,8 +41,22 @@ function Details({ dataOfDetails, path }) {
           alt="recipe-img"
         />
         <h1 data-testid="recipe-title">{dataOfDetails.strDrink}</h1>
-        <button type="button" data-testid="share-btn">compartilhar</button>
-        <button type="button" data-testid="favorite-btn">favoritar</button>
+        {btnShareshow ? (
+          <input
+            type="image"
+            onClick={ () => { clickShareRecipe(dataOfDetails); } }
+            data-testid="share-btn"
+            src={ shareIcon }
+            alt="compartilhar"
+          />) : (<button type="button">Link copied!</button>
+        )}
+        <input
+          type="image"
+          data-testid="favorite-btn"
+          alt="favoritar"
+          src={ whiteHeartIcon }
+          onClick={ () => { clickFavoriteRecipe(); } }
+        />
         <p data-testid="recipe-category">{dataOfDetails.strAlcoholic}</p>
         <IngredientsList dataDetails={ dataOfDetails } />
         <div>
@@ -31,8 +74,22 @@ function Details({ dataOfDetails, path }) {
           alt="recipe-img"
         />
         <h1 data-testid="recipe-title">{dataOfDetails.strMeal}</h1>
-        <button type="button" data-testid="share-btn">compartilhar</button>
-        <button type="button" data-testid="favorite-btn">favoritar</button>
+        { btnShareshow ? (
+          <input
+            type="image"
+            onClick={ () => { clickShareRecipe(dataOfDetails); } }
+            data-testid="share-btn"
+            src={ shareIcon }
+            alt="compartilhar"
+          />) : (<button type="button">Link copied!</button>
+        )}
+        <input
+          type="image"
+          data-testid="favorite-btn"
+          alt="favoritar"
+          src={ whiteHeartIcon }
+          onClick={ () => { clickFavoriteRecipe(); } }
+        />
         <h3 data-testid="recipe-category">{dataOfDetails.strCategory}</h3>
         <IngredientsList dataDetails={ dataOfDetails } />
         <div>
